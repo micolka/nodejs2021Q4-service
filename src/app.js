dc2a8c8e6c26a2cbf707615a1641ebbbf17adea9
micolka <mic_la@tut.by>
2021-12-05 10:12:11 +0300

    feat: fastify swagger migration

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-// const express = require('express');
-// const swaggerUI = require('swagger-ui-express');
-// const path = require('path');
-// const YAML = require('yamljs');
+const path = require('path');
 const app = require('fastify')({logger: true})
 
 app.get('/', async () => ({ hello: 'world'}))
 
-
-// const app = express();
-// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-// app.use(express.json());
-
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.register(require('fastify-swagger'), {
+  mode: 'static',
+  specification: {
+      path: path.join(__dirname, '../doc/api.yaml'),
+  },
+  exposeRoute: true,
+  routePrefix: '/doc',
+})
 
 app.register(require('./resources/users/user.router'))
 app.register(require('./resources/board/board.router'))
