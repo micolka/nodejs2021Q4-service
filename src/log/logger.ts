9c595d1f8c52ec8901a421a67cbdef768809c7bf
micolka <mic_la@tut.by>
2021-12-26 21:22:45 +0300

    feat: add process error handling

--- src/log/logger.ts
+++ src/log/logger.ts
@@ -1,8 +1,10 @@
-/* eslint-disable class-methods-use-this */
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify"
 import fs from "fs"
 import * as path from 'path'
 
+const writeInfoToFile = (data: string) => {
+  fs.appendFileSync(path.join(__dirname, './log.txt'), data)    
+}
 
 export class Logger {
   constructor(app: FastifyInstance) {
@@ -17,7 +19,7 @@ export class Logger {
 
       const outStr = `WARN [message]: ${error.message}, [time]: ${Date.now()}, [url]: ${url}, [params]: ${JSON.stringify(params)}, [code]: ${statusCode}, [body]: ${JSON.stringify(body)}\n`
       console.warn(outStr)
-      this.writeInfoToFile(outStr)
+      writeInfoToFile(outStr)
       rep.status(404).send({ ok: false })
     })
   }
@@ -28,10 +30,20 @@ export class Logger {
 
     const outStr = `LOG [time]: ${Date.now()}, [url]: ${url}, [params]: ${JSON.stringify(params)}, [code]: ${statusCode}, [body]: ${JSON.stringify(body)}\n`
     console.log(outStr)
-    this.writeInfoToFile(outStr)
+    writeInfoToFile(outStr)
   }
 
-  private writeInfoToFile(data: string) {
-    fs.appendFileSync(path.join(__dirname, './log.txt'), data)    
+  static logUncaughtException(err: Error, origin: NodeJS.UncaughtExceptionOrigin) {
+    const outStr = `ERROR [time]: ${Date.now()}, [Caught exception]: ${err}, [Exception origin]: ${origin}\n`
+    console.error(outStr)
+    writeInfoToFile(outStr)
+    process.exit(0)
+  }
+
+  static logUnhandledRejection(reason: unknown, promise: Promise<unknown>) {
+    const outStr = `ERROR [time]: ${Date.now()}, [Unhandled Rejection at]: ${promise}, [reason]: ${reason}\n`
+    console.error(outStr)
+    writeInfoToFile(outStr)
+    process.exit(0)
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import app from './app'
 import appSettings from './common/config'
+import { Logger } from './log/logger'
 
 const { PORT } = appSettings
  
 app.listen(PORT as string, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 )
+process.on('uncaughtException', Logger.logUncaughtException)
+
+// throw Error('Oops!');
+
+process.on('unhandledRejection', Logger.logUnhandledRejection)
+
+// Promise.reject(Error('Oops!'));
